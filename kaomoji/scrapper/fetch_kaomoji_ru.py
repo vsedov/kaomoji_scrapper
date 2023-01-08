4d2e762d78e748678e9395681b1fb6dd67f4714f
vsedov <viv.sedov@hotmail.com>
2023-01-08 06:35:16 +0000

    Feat: 1.Project Creation       2. Fetch kaomoji from Kaomoji.ru

---
+++ kaomoji/scrapper/fetch_kaomoji_ru.py
@@ -0,0 +1,66 @@
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+from kaomoji.utils import project_path
+
+
+def get_kaomojis(table):
+    """Get kaomijs
+
+    Parameters
+    ----------
+    table : parse in Page elements
+        Table contains "TD" We want to filter that out
+
+    Returns
+    -------
+    List of TD if it exists
+    """
+    return [(td.text, None) for td in table.find_all("td") if td.text]
+
+
+def get_kaomojis_special(table):
+    """There is a special section in this website
+    That would need to be addressed, this little function
+    will help identify and split the text and description up.
+
+    Parameters
+    ----------
+    table : Page elements table
+
+    Returns
+    -------
+    List of tuples: The emoji it self and the text
+    """
+    return [
+        (kaomij.text, desc.text)
+        for kaomij, desc in zip(
+            table.find_all("td")[::2], table.find_all("td")[1::2]
+        )
+    ]
+
+
+def create_tsv():
+    tsv_path = f"{project_path()}/data/kaomojis_1.tsv"
+    response = urllib.request.urlopen("http://kaomoji.ru/en/")
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+    emoji_dict = {}
+    for kaomoji_table in soup.find_all("table", class_="table_kaomoji"):
+        name = kaomoji_table.find_parent("div").find_previous_sibling("h3").text
+
+        if name == "Special":
+            emoji_dict[name] = get_kaomojis_special(kaomoji_table)
+        else:
+            emoji_dict[name] = get_kaomojis(kaomoji_table)
+
+    with open(tsv_path, "w") as fp:
+        fp.write("category\tkaomoji\tdescription\n")
+        for id, (category, els) in enumerate(emoji_dict.items()):
+            i = 0
+            for el in els:
+                kao, desc = el
+                desc = desc or ""
+                fp.write(f"{kao}\t{category}_{i}\t{desc}\n")
+                i += 1
---
+++ kaomoji/utils.py
@@ -0,0 +1,5 @@
+import os
+
+
+def project_path():
+    return os.path.dirname(os.path.abspath(__file__))
